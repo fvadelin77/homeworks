ed22ec2bc966e8b86dfc4ab7d88e7a0739e7bfc0
fvadelin77 <fvadelin2@gmail.com>
2022-03-29 15:42:47 +0300

    JS part 2

--- TemaJsPart2/script.js
+++ TemaJsPart2/script.js
@@ -1,4 +1,6 @@
+"use strict";
 // 1. O functie "getDigits" care primeste un sir de caractere si returneaza cifrele din sirul respectiv
+// All good
 function getDigits(str) {
   let strNo = "";
   for (let i = 0; i < str.length; i++) {
@@ -8,33 +10,32 @@ function getDigits(str) {
   return strNo;
 }
 console.log(getDigits("xx56xx456xxxxx456xx456"));
-// const test = "1" * 1;
-// console.log(test, typeof test);
 
 // 2. O functie "getLetters" care primeste un sir de caractere si returneaza doar literele din sirul respectiv // de modificat
+// All good
 function getLetters(str) {
   let strTxt = "";
   for (let i = 0; i < str.length; i++) {
-    if (!isNaN(str[i])) continue;
-    strTxt += str[i];
+    if ((str[i] >= "A" && str[i] <= "Z") || (str[i] >= "a" && str[i] <= "z")) {
+      strTxt += str[i];
+    }
   }
   return strTxt;
 }
 console.log(getLetters("xx56xx456xxxxx456xx456"));
 
-// 3. O functie "getFirst5Letters" care primeste un sir de caractere si returneaza primele 5 litere(daca exista) // de modificat 1
+// 3. O functie "getFirst5Letters" care primeste un sir de caractere si returneaza primele 5 litere(daca exista)
+// All good
 function getFirst5Letters(str) {
-  let strTxt = "";
-  for (let i = 0; i < str.length; i++) {
-    if (!isNaN(str[i])) continue;
-    if (strTxt.length <= 4) {
-      strTxt += str[i];
-    } else break;
+  let output = "";
+  let onlyLettersStr = getLetters(str);
+  for (let i = 0; output.length < 5; i++) {
+    output += onlyLettersStr[i];
   }
-  return strTxt;
+  return output;
 }
 
-console.log(getFirst5Letters("as123dfgh123jkl"), getFirst5Letters("abc123"));
+console.log(getFirst5Letters("as123dfgh123jkl"), getFirst5Letters("abccc1223"));
 
 // 4. O functie "concatenate" care primeste o lista de siruri de caractere si returneaza sirurile concatenate - all good!
 function concatenate(arr) {
@@ -130,21 +131,129 @@ function palindrom(a) {
 console.log(palindrom(1234321));
 
 // 12. O functie care sorteaza numerele pare dintr-un sir de numere primit ca parametru. ("sort") - de verificat pt numere negative
-function sort(a) {
-  let aPar = [];
-  for (let i = 0; i < a.length; i++)
-    while (a[i] > 0) {
-      if ((a[i] % 10) % 2 === 0) {
-        aPar.push(a[i] % 10);
+function sort(arr) {
+  // sortare array bubble
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
-      a[i] = Math.trunc(a[i] / 10);
     }
-  return aPar;
+  }
+  // scriu ouput cu valorile sortate, pare
+  let sorted = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sorted.push(arr[i]);
+    }
+  }
+  return sorted;
 }
-console.log(sort([1, -2, 3, 4]));
-// 13. O functie care primeste ca parametru un array de numere. Aceasta sorteaza ascendent numerele pare si descendent numerele impare, in cadrul aceluiasi array primit ca parameru. ("sortAscDesc")
+console.log(sort([5, 4, 3, 2, 1]));
 
-// if nr par, push la array
+// 13. O functie care primeste ca parametru un array de numere. Aceasta sorteaza ascendent numerele pare si descendent numerele impare, in cadrul aceluiasi array primit ca parameru. ("sortAscDesc")
+function sortAscDesc(arr) {
+  // sortez array bubble
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  // push ouput Asc - par
+  let sorted = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sorted.push(arr[i]);
+    }
+  }
+  // scriu ouput Desc - impar
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] % 2 !== 0) {
+      sorted.push(arr[i]);
+    }
+  }
+  return sorted;
+}
+console.log(sortAscDesc([5, 4, 3, 2, 1]));
 
 // 14. O functie care primeste 2 parametri(un array si un numar). Folosind binary search verificati daca numarul primit ca parametru se gaseste in array. ("binarySearch")
+function sortBubble(arr) {
+  // sortare array bubble
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+function binarySearch(arr, val) {
+  let min = sortBubble(arr)[0];
+  let max = sortBubble(arr)[arr.length - 1];
+  while (min <= max) {
+    let middle = Math.floor((max + min) / 2);
+    if (middle === val) {
+      return true;
+    } else if (val < middle) {
+      max = middle - 1;
+    } else if (val > middle) {
+      min = middle + 1;
+    }
+  }
+  return false;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 2));
+
 // 15. O functie care implementeaza binary search pentru a verifica daca un numar se regaseste intr-un array. Dupa ce se termina executia functiei trebuie sa returnati de cate ori s-a apelat functia recursiv ("countBinarySearch")
+function countBinarySearch(arr, val) {
+  let min = sortBubble(arr)[0];
+  let max = sortBubble(arr)[arr.length - 1];
+  let counter = 0;
+  while (min <= max) {
+    let middle = Math.floor((max + min) / 2);
+    counter++;
+    if (middle === val) {
+      return counter;
+    } else if (val < middle) {
+      max = middle - 1;
+    } else if (val > middle) {
+      min = middle + 1;
+    }
+  }
+  return counter;
+}
+
+console.log(countBinarySearch([1, 2, 3, 4, 5], 5));
+
+// 15.
+// function countBinarySearch1(arr, val) {
+//   let min = sortBubble(arr)[0];
+//   let max = sortBubble(arr)[arr.length - 1];
+//   let middle = Math.floor((min + max) / 2);
+//   let counter = 1;
+//   if (val >= min && val <= max) {
+//     while (middle !== val) {
+//       if (val < middle) {
+//         max = middle - 1;
+//         middle = Math.floor((min + max) / 2);
+//       } else if (val > middle) {
+//         min = middle + 1;
+//         middle = Math.floor((min + max) / 2);
+//       }
+//     }
+//     return true;
+//   }
+//   return false;
+// }
+
+// console.log(countBinarySearch1([1, 2, 3, 4, 5]));
