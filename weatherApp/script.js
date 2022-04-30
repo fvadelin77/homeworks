8e370ae973dfd06cc13eab263e348d4802accc86
fvadelin77 <fvadelin2@gmail.com>
2022-05-01 01:54:41 +0300

    added daily forecast

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -8,12 +8,13 @@ let state = {
 };
 
 async function getCurrentData() {
-  let location = document.querySelector("input[name=location-input]");
+  let location = document.querySelector("input[name=location-input]").value;
+  let locationTrimmed = location.trim();
   let loader = document.querySelector(".lds-ring");
   let warning = document.querySelector(".warning");
-  let letters = /^[A-Za-z]+$/;
+  let letters = /^[A-Za-z-]+$/;
   loader.classList.toggle("hidden");
-  if (!location.value) {
+  if (!location) {
     warning.classList.remove("hidden");
     loader.classList.toggle("hidden");
     warning.value = "Location cannot be empty !";
@@ -22,51 +23,53 @@ async function getCurrentData() {
     }, 3000);
     return;
   }
-  if (!location.value.match(letters)) {
+  if (!locationTrimmed.match(letters)) {
     warning.classList.remove("hidden");
     loader.classList.toggle("hidden");
     warning.value = "Only alphabetical characters allowed !";
-    location.value = "";
+    location = "";
     setTimeout(function () {
       warning.classList.add("hidden");
     }, 3000);
     return;
   } else {
     let responseCurrent = await fetch(
-      state.urlCurrent + `${location.value.toLowerCase()}`
+      state.urlCurrent + `${locationTrimmed.toLowerCase()}`
     );
     if (responseCurrent.status === 404) {
       warning.classList.remove("hidden");
       loader.classList.toggle("hidden");
-      warning.value = `Location "${location.value}", not found !`;
-      location.value = "";
+      warning.value = `Location "${locationTrimmed}", not found !`;
+      location = "";
       setTimeout(function () {
         warning.classList.add("hidden");
       }, 3000);
       return;
+    } else {
+      state.current = await responseCurrent.json();
+      state.icon = await fetch(
+        state.urlIcon + `${state.current.weather[0].icon}` + ".png"
+      );
+      loader.classList.toggle("hidden");
+      populateCurrent();
+      getForecastedData();
     }
-    state.current = await responseCurrent.json();
-    state.icon = await fetch(
-      state.urlIcon + `${state.current.weather[0].icon}` + ".png"
-    );
-    loader.classList.toggle("hidden");
-    console.log(state.current, state.icon);
-    location.value = "";
-    populateCurrent();
   }
 }
 
 async function getForecastedData() {
   let location = document.querySelector("input[name=location-input]");
-  if (!location.value) {
+  let locationTrimmed = location.value.trim();
+  if (!locationTrimmed) {
     return;
   } else {
     let responseForecast = await fetch(
-      state.urlForecast + `${location.value.toLowerCase()}`
+      state.urlForecast + `${locationTrimmed.toLowerCase()}`
     );
     state.forecast = await responseForecast.json();
   }
-  console.log(state.forecast);
+  populateDailyForecast();
+  location.value = "";
 }
 
 function populateCurrent() {
@@ -113,58 +116,9 @@ function getSecondaryData() {
   }
   rain.innerHTML = `Humidity: ${state.current.main.humidity}%`;
   wind.innerHTML = `Wind: ${windFormated} km/h`;
+
   initMap();
 }
-
-// {
-//     "coord": {
-//         "lon": 24.7022,
-//         "lat": 46.7742
-//     },
-//     "weather": [
-//         {
-//             "id": 500,
-//             "main": "Rain",
-//             "description": "light rain",
-//             "icon": "10n"
-//         }
-//     ],
-//     "base": "stations",
-//     "main": {
-//         "temp": 11.85,
-//         "feels_like": 11.36,
-//         "temp_min": 11.51,
-//         "temp_max": 11.85,
-//         "pressure": 1019,
-//         "humidity": 87,
-//         "sea_level": 1019,
-//         "grnd_level": 975
-//     },
-//     "visibility": 10000,
-//     "wind": {
-//         "speed": 1.77,
-//         "deg": 76,
-//         "gust": 1.84
-//     },
-//     "rain": {
-//         "1h": 0.21
-//     },
-//     "clouds": {
-//         "all": 93
-//     },
-//     "dt": 1651096241,
-//     "sys": {
-//         "type": 2,
-//         "id": 50402,
-//         "country": "RO",
-//         "sunrise": 1651115478,
-//         "sunset": 1651166751
-//     },
-//     "timezone": 10800,
-//     "id": 668997,
-//     "name": "Reghin",
-//     "cod": 200
-// }
 
 function initMap() {
   if (state.current === undefined) {
@@ -182,3 +136,35 @@ function initMap() {
     let map = new google.maps.Map(document.getElementById("map"), mapProp);
   }
 }
+
+function populateDailyForecast() {
+  let elem = document.querySelector(".forecastDaily");
+  let list = state.forecast.list;
+  let str = "";
+  for (let i = 0; i < 12; i++) {
+    let date = new Date(list[i].dt * 1000);
+    let dayOfWeek = date.toLocaleDateString("en-US", { weekday: "short" });
+    let icon = list[i].weather[0].icon;
+    console.log(icon);
+    console.log(`${dayOfWeek}, ${date.getDate()}/${date.getMonth() + 1}`);
+    str += `
+        <div class="forecastElemDaily">
+            <div class="date">${dayOfWeek}, ${date.getDate()}/${
+      date.getMonth() + 1
+    }
+            </div>
+            <div class="hour">${date.getHours()}:${
+      date.getMinutes() + "0"
+    }</div>
+        <div class="icon-temp">
+            <img src = "${state.urlIcon + icon}.png"/>
+            <div class="temp">${Math.trunc(list[i].main.temp)} °C</div>
+        </div>
+        </div>
+    `;
+    elem.innerHTML = str;
+    document.querySelector(".forecastDaily").classList.remove("hidden");
+  }
+}
+
+// de adaugat conditii si la input (forecast)
