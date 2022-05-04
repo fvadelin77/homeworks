9376bb935f5907da33d1ff155f1275033be70304
fvadelin77 <fvadelin2@gmail.com>
2022-05-04 14:17:51 +0300

    done weatherApp i think

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -12,22 +12,13 @@ async function getCurrentData() {
   let locationTrimmed = location.trim();
   let loader = document.querySelector(".lds-ring");
   let warning = document.querySelector(".warning");
-  let letters = /^[A-Za-z-]+$/;
+  //   let letters = /^[A-Za-z-]+$/;
+
   loader.classList.toggle("hidden");
   if (!location) {
     warning.classList.remove("hidden");
     loader.classList.toggle("hidden");
     warning.value = "Location cannot be empty !";
-    setTimeout(function () {
-      warning.classList.add("hidden");
-    }, 3000);
-    return;
-  }
-  if (!locationTrimmed.match(letters)) {
-    warning.classList.remove("hidden");
-    loader.classList.toggle("hidden");
-    warning.value = "Only alphabetical characters allowed !";
-    location = "";
     setTimeout(function () {
       warning.classList.add("hidden");
     }, 3000);
@@ -51,46 +42,72 @@ async function getCurrentData() {
         state.urlIcon + `${state.current.weather[0].icon}` + ".png"
       );
       loader.classList.toggle("hidden");
-      populateCurrent();
-      getForecastedData();
+      await getForecastedData();
     }
   }
-}
-
-async function getForecastedData() {
-  let location = document.querySelector("input[name=location-input]");
-  let locationTrimmed = location.value.trim();
-  if (!locationTrimmed) {
-    return;
-  } else {
-    let responseForecast = await fetch(
-      state.urlForecast + `${locationTrimmed.toLowerCase()}`
-    );
-    state.forecast = await responseForecast.json();
-  }
-  populateDailyForecast();
-  location.value = "";
+  populateCurrent();
 }
 
 function populateCurrent() {
   getDateTime();
   getSecondaryData();
+  let locationInput = document.querySelector("input[name=location-input]");
   let cr = state.current;
+  console.log(cr);
   let crWeather = cr.weather[0];
   let location = document.querySelector(".location-icon");
   let celsius = document.querySelector(".celsius");
-  let min = document.querySelector(".min-temp");
   let time = document.querySelector(".time");
+  let min = document.querySelector(".min-temp");
   location.innerHTML = `
     <div class = "location">${cr.name}, ${cr.sys.country}</div>
     <img src = "${state.urlIcon + crWeather.icon}.png"/>
-  `;
-  celsius.innerHTML = `${Math.trunc(cr.main.temp)}°C`;
-  min.innerHTML = `Min / Max temp.: ${Math.trunc(
+    <div class="descr">${crWeather.description}</div>
+    `;
+  celsius.innerHTML = `${Math.round(cr.main.temp)}°C`;
+  min.innerHTML = `Min / Max temp.: ${Math.round(
     cr.main.temp_min
-  )}°C / ${Math.trunc(cr.main.temp_max)}°C`;
+  )}°C / ${Math.round(cr.main.temp_max)}°C`;
   time.innerHTML = state.timeDayOfWeek;
   document.querySelector(".current-info").classList.remove("hidden");
+
+  // Daily forecast
+  let elem = document.querySelector(".forecastDaily");
+  let list = state.forecast.list;
+  let str = "";
+  for (let i = 0; i < 9; i++) {
+    let date = new Date(list[i].dt * 1000);
+    let dayOfWeek = date.toLocaleDateString("en-US", { weekday: "short" });
+    let icon = list[i].weather[0].icon;
+
+    str += `
+            <div class="forecastElemDaily">
+            <div class="date-hour">
+            <div class="date">${dayOfWeek}, ${date.getDate()}/${
+      date.getMonth() + 1
+    }
+            </div>
+            <div class="hour">${date.getHours()}:${
+      date.getMinutes() + "0"
+    }</div>
+            </div>
+            <div class="icon-temp">
+            <img src = "${state.urlIcon + icon}.png"/>
+            <div class="temp">${Math.round(list[i].main.temp)} °C</div>
+            </div>
+            ${
+              list[i].rain === undefined
+                ? ""
+                : `<div class="rainProb">Rain: ${Math.round(
+                    list[i].pop * 100
+                  )}%</div>`
+            }
+                </div>
+                `;
+  }
+  elem.innerHTML = str;
+  locationInput.value = "";
+  elem.classList.remove("hidden");
 }
 
 function getDateTime() {
@@ -101,7 +118,8 @@ function getDateTime() {
 }
 
 function getSecondaryData() {
-  let precip = document.querySelector(".precip");
+  // state.current.main.pressure
+  let pressure = document.querySelector(".pressure");
   let rain = document.querySelector(".rain");
   let wind = document.querySelector(".wind");
   let windRaw = state.current.wind.speed * 3.6;
@@ -109,11 +127,12 @@ function getSecondaryData() {
     maximumFractionDigits: 2,
     minimumFractionDigits: 2,
   });
-  if (!state.current.rain) {
-    precip.innerHTML = "Rain 1h: 0 mm/km²";
-  } else {
-    precip.innerHTML = `Rain (1h): ${state.current.rain["1h"]} mm/km²`;
-  }
+  //   if (!state.current.rain) {
+  //     precip.innerHTML = "Rain 1h: 0 mm/km²";
+  //   } else {
+  //     precip.innerHTML = `Rain (1h): ${state.current.rain["1h"]} mm/km²`;
+  //   }
+  pressure.innerHTML = `Pressure: ${state.current.main.grnd_level} hPa`;
   rain.innerHTML = `Humidity: ${state.current.main.humidity}%`;
   wind.innerHTML = `Wind: ${windFormated} km/h`;
 
@@ -137,41 +156,72 @@ function initMap() {
   }
 }
 
-function populateDailyForecast() {
-  let elem = document.querySelector(".forecastDaily");
-  let list = state.forecast.list;
+function populateForecast() {
+  let elem = document.querySelector(".forecast");
+  let warning = document.querySelector(".warning");
   let str = "";
+  //   let dateInitial = new Date(list[0].dt * 1000);
+  //   let dayOfWeek = date.toLocaleDateString("en-US", { weekday: "short" });
+  if (state.current === undefined) {
+    warning.classList.remove("hidden");
+    warning.value = 'Get "Current weather" first';
+    setTimeout(function () {
+      warning.classList.add("hidden");
+    }, 3000);
+    return;
+  }
+  let list = state.forecast.list;
   for (let i = 0; i < list.length; i++) {
     let date = new Date(list[i].dt * 1000);
     let dayOfWeek = date.toLocaleDateString("en-US", { weekday: "short" });
     let icon = list[i].weather[0].icon;
-    console.log(state.forecast);
-
     str += `
-        <div class="forecastElemDaily">
-        <div class="date-hour">
-            <div class="date">${dayOfWeek}, ${date.getDate()}/${
-      date.getMonth() + 1
-    }
-            </div>
-            <div class="hour">${date.getHours()}:${
-      date.getMinutes() + "0"
-    }</div>
+    <div class="forecastElem">
+    <div class="date-hour">
+    <div class="date">${dayOfWeek}, ${date.getDate()}/${date.getMonth() + 1}
     </div>
-        <div class="icon-temp">
-            ${
-              list[i].rain === undefined
-                ? ""
-                : `<div class="rainProb">${list[i].pop * 100}%</div>`
-            }
-            <img src = "${state.urlIcon + icon}.png"/>
-            <div class="temp">${Math.trunc(list[i].main.temp)} °C</div>
-        </div>
-        </div>
-    `;
-    elem.innerHTML = str;
-    document.querySelector(".forecastDaily").classList.remove("hidden");
+    <div class="hour">${date.getHours()}:${date.getMinutes() + "0"}</div>
+    </div>
+    <div><i>${list[i].weather[0].description}</i></div>
+    <div class="icon-temp">
+    ${
+      list[i].rain === undefined
+        ? ""
+        : `<div class="rainProb">${Math.round(list[i].pop * 100)}%</div>`
+    }
+        <img src = "${state.urlIcon + icon}.png"/>
+        <div class="temp">${Math.round(list[i].main.temp)} °C</div>
+            </div>
+            </div>
+            `;
+  }
+  console.log(state.forecast);
+  elem.innerHTML = str;
+  document.querySelector(".forecast").classList.remove("hidden");
+}
+
+async function getForecastedData() {
+  let location = document.querySelector("input[name=location-input]");
+  let locationTrimmed = location.value.trim();
+  if (!locationTrimmed) {
+    return;
+  } else {
+    let responseForecast = await fetch(
+      state.urlForecast + `${locationTrimmed.toLowerCase()}`
+    );
+    state.forecast = await responseForecast.json();
   }
 }
 
-// de adaugat conditii si la input (forecast)
+// function isLooseEquals (val1,val2) {
+// if (val1 === val2) {
+//   return true;
+// }
+// if (
+//   (val1 === null || val1 === undefined) &&
+//   (val2 === null || val2 === undefined)
+// ) {
+// }
+// }
+
+// retrive data
